44634a4ba67914ef3d577f35dbde232d553ccb83
ESTRADA Nicolas <nicolas.estrada@oscaro.com>
2019-11-14 14:00:30 +0100

    Bumped dependencies and google java libraries

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,13 @@
 # clj-gcloud-common
 
+## 0.112-1.0 (2019/11/14)
+
+Bumped dependencies and google java libraries
+
+## 0.87-1.0 (2019/06/03)
+
+New java libs; Durations are now expressed as tuples; Minor refactor
+
 ## 0.71-1.1 (2019/04/17)
 
 First public release.
--- project.clj
+++ project.clj
@@ -1,26 +1,30 @@
-(defproject com.oscaro/clj-gcloud-common "0.87-1.0"
+(defproject com.oscaro/clj-gcloud-common "0.112-1.0"
   :description "Common library for all google cloud clojure wrappers"
   :url "https://github.com/oscaro/clj-gcloud-common"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.0" :scope "provided"]
-                 [cheshire "5.8.1"]
-                 [com.google.cloud/google-cloud-core "1.69.0"]
-                 [com.google.cloud/google-cloud-core-http "1.69.0"]
+  :dependencies [[org.clojure/clojure "1.10.1" :scope "provided"]
+                 [cheshire "5.9.0"]
+                 [com.google.cloud/google-cloud-core "1.91.3"]
+                 [com.google.cloud/google-cloud-core-http "1.91.3"]
                  ;; The dependencies below were carefully added and excluded
                  ;; in order to remove all confusing warnings.
-                 [com.google.guava/guava "27.1-jre"]
-                 [io.grpc/grpc-core "1.19.0"]
+                 [com.google.code.gson/gson "2.8.5"]
+                 [com.google.guava/guava "28.1-jre"]
+                 [io.grpc/grpc-api "1.24.1"]
+                 [io.grpc/grpc-core "1.24.1" :exclusions [com.google.errorprone/error_prone_annotations
+                                                          io.grpc/grpc-api]]
                  [com.google.errorprone/error_prone_annotations "2.3.3"]]
   :profiles
   {:dev
-   {:dependencies   [[com.google.cloud/google-cloud-bigquery "1.69.0" ]
-                     [com.google.cloud/google-cloud-pubsub "1.69.0" :exclusions [io.grpc/grpc-core
-                                                                                 io.grpc/grpc-netty-shaded]]
-                     [com.google.cloud/google-cloud-storage "1.69.0"]
-                     [com.google.cloud/google-cloud-datastore "1.69.0"]
-                     [org.clojure/tools.namespace "0.2.11"]
-                     [io.grpc/grpc-netty-shaded "1.19.0" :exclusions [io.grpc/grpc-core]]]
+   {:dependencies   [[com.google.cloud/google-cloud-bigquery "1.100.0"]
+                     [com.google.cloud/google-cloud-pubsub "1.100.0" :exclusions [io.grpc/grpc-api
+                                                                                  io.grpc/grpc-core
+                                                                                  io.grpc/grpc-netty-shaded]]
+                     [com.google.cloud/google-cloud-storage "1.100.0"]
+                     [com.google.cloud/google-cloud-datastore "1.100.0" :exclusions [io.grpc/grpc-api
+                                                                                     io.grpc/grpc-core]]
+                     [org.clojure/tools.namespace "0.3.1"]]
     :source-paths   ["dev"]
     :resource-paths ["test-resources"]
     :test-selectors {:ci (complement :integration)}}}
