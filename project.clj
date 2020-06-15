a34c101ff10a5ceccdd7524c98d3e0546091dc1d
Baptiste Fontaine <baptiste.fontaine@oscaro.com>
2020-06-15 12:49:17 +0200

    CI: switch to GitHub Actions

--- project.clj
+++ project.clj
@@ -1,8 +1,16 @@
-(defproject com.oscaro/clj-gcloud-common "0.112-1.0"
+(defproject com.oscaro/clj-gcloud-common "0.112-1.1-SNAPSHOT"
   :description "Common library for all google cloud clojure wrappers"
   :url "https://github.com/oscaro/clj-gcloud-common"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :deploy-repositories [["snapshots" {:url "https://repo.clojars.org"
+                                      :username :env/clojars_username
+                                      :password :env/clojars_password
+                                      :sign-releases false}]
+                        ["releases"  {:url "https://repo.clojars.org"
+                                      :username :env/clojars_username
+                                      :password :env/clojars_password
+                                      :sign-releases false}]]
   :dependencies [[org.clojure/clojure "1.10.1" :scope "provided"]
                  [cheshire "5.9.0"]
                  [com.google.cloud/google-cloud-core "1.91.3"]
