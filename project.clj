2522ad9ecf59397b32fd6dd26976398d19d2b85a
Ed Porras <dev@digressed.net>
2021-03-31 16:37:58 +0200

    update base dependencies

--- project.clj
+++ project.clj
@@ -9,10 +9,9 @@
                                       :sign-releases false}]
                         ["releases"  {:url "https://repo.clojars.org"
                                       :creds :gpg}]]
-  :dependencies [[org.clojure/clojure "1.10.1" :scope "provided"]
-                 [cheshire "5.9.0"]
-                 [com.google.cloud/google-cloud-core "1.91.3"]
-                 [com.google.cloud/google-cloud-core-http "1.91.3"]
+  :dependencies [[org.clojure/clojure "1.10.3" :scope "provided"]
+                 [com.google.cloud/google-cloud-core "1.94.4"]
+                 [com.google.cloud/google-cloud-core-http "1.94.4"]
                  ;; The dependencies below were carefully added and excluded
                  ;; in order to remove all confusing warnings.
                  [com.google.code.gson/gson "2.8.5"]
@@ -23,14 +22,14 @@
                  [com.google.errorprone/error_prone_annotations "2.3.3"]]
   :profiles
   {:dev
-   {:dependencies   [[com.google.cloud/google-cloud-bigquery "1.100.0"]
-                     [com.google.cloud/google-cloud-pubsub "1.100.0" :exclusions [io.grpc/grpc-api
+   {:dependencies   [[com.google.cloud/google-cloud-bigquery "1.127.11"]
+                     [com.google.cloud/google-cloud-pubsub "1.112.0" :exclusions [io.grpc/grpc-api
                                                                                   io.grpc/grpc-core
                                                                                   io.grpc/grpc-netty-shaded]]
-                     [com.google.cloud/google-cloud-storage "1.100.0"]
-                     [com.google.cloud/google-cloud-datastore "1.100.0" :exclusions [io.grpc/grpc-api
+                     [com.google.cloud/google-cloud-storage "1.113.14"]
+                     [com.google.cloud/google-cloud-datastore "1.106.0" :exclusions [io.grpc/grpc-api
                                                                                      io.grpc/grpc-core]]
-                     [org.clojure/tools.namespace "1.0.0"]]
+                     [org.clojure/tools.namespace "1.1.0"]]
     :source-paths   ["dev"]
     :resource-paths ["test-resources"]
     :test-selectors {:ci (complement :integration)}}}
