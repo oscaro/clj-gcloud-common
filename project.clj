e582ffdd690c4be0488a9bbc63795ba5fac5f947
Mathieu Lirzin <mathieu.lirzin@oscaro.com>
2021-04-04 23:23:11 +0200

    build: Use dependency versions defined in latest BOM files

--- project.clj
+++ project.clj
@@ -9,27 +9,35 @@
                                       :sign-releases false}]
                         ["releases"  {:url "https://repo.clojars.org"
                                       :creds :gpg}]]
-  :dependencies [[org.clojure/clojure "1.10.3" :scope "provided"]
-                 [com.google.cloud/google-cloud-core "1.94.4"]
-                 [com.google.cloud/google-cloud-core-http "1.94.4"]
-                 ;; The dependencies below were carefully added and excluded
-                 ;; in order to remove all confusing warnings.
-                 [com.google.code.gson/gson "2.8.6"]
-                 [com.google.guava/guava "29.0-jre"]
-                 [io.grpc/grpc-api "1.36.1"]
-                 [io.grpc/grpc-core "1.36.1" :exclusions [com.google.errorprone/error_prone_annotations
-                                                          io.grpc/grpc-api]]
-                 [com.google.errorprone/error_prone_annotations "2.5.1"]]
+  :managed-dependencies
+  ;; Google “Bill of Materials” (BOM) defines a combination of
+  ;; dependency versions that work well with each other.
+  [[com.google.cloud/google-cloud-bom "0.150.0"
+    :extension "pom"
+    :scope "import"]
+   [com.google.cloud/google-cloud-shared-dependencies "0.20.1"
+    :extension "pom"
+    :scope "import"]
+   ;; Handle version mismatches between google-cloud-bom and
+   ;; google-cloud-shared-dependencies.
+   [com.google.protobuf/protobuf-java "3.15.3"]
+   [com.google.protobuf/protobuf-java-util "3.15.3"]
+   ;; Select non-android Guava to work around Guava versioning mess
+   ;; <https://github.com/google/guava/issues/2914>.
+   [com.google.guava/guava "30.1-jre"]]
+  :dependencies
+  [[org.clojure/clojure "1.10.3" :scope "provided"]
+   ;; This must correspond to the version pinned in BOM files.
+   [com.google.cloud/google-cloud-core "1.94.3"]
+   [com.google.cloud/google-cloud-core-http "1.94.3"]]
   :profiles
   {:dev
-   {:dependencies   [[com.google.cloud/google-cloud-bigquery "1.127.11"]
-                     [com.google.cloud/google-cloud-pubsub "1.112.0" :exclusions [io.grpc/grpc-api
-                                                                                  io.grpc/grpc-core
-                                                                                  io.grpc/grpc-netty-shaded]]
-                     [com.google.cloud/google-cloud-storage "1.113.14"]
-                     [com.google.cloud/google-cloud-datastore "1.106.0" :exclusions [io.grpc/grpc-api
-                                                                                     io.grpc/grpc-core]]
-                     [org.clojure/tools.namespace "1.1.0"]]
+   {:dependencies
+    [[com.google.cloud/google-cloud-bigquery "1.127.8"]
+     [com.google.cloud/google-cloud-pubsub "1.112.0"]
+     [com.google.cloud/google-cloud-storage "1.113.14"]
+     [com.google.cloud/google-cloud-datastore "1.106.0"]
+     [org.clojure/tools.namespace "1.1.0"]]
     :source-paths   ["dev"]
     :resource-paths ["test-resources"]
     :test-selectors {:ci (complement :integration)}}}
