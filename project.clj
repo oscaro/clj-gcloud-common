9f7203de1a636a4009db7a3998ed57d5e3a5feb5
Ed Porras <dev@digressed.net>
2022-04-23 11:48:58 +0200

    bump deps w/ gc-bom 0.172.0 as base

--- project.clj
+++ project.clj
@@ -12,31 +12,32 @@
   :managed-dependencies
   ;; Google “Bill of Materials” (BOM) defines a combination of
   ;; dependency versions that work well with each other.
-  [[com.google.cloud/google-cloud-bom "0.164.0"
+  [[com.google.cloud/google-cloud-bom "0.172.0"
     :extension "pom"
     :scope "import"]
-   [com.google.cloud/google-cloud-shared-dependencies "2.5.1"
+   [com.google.cloud/google-cloud-shared-dependencies "2.10.0"
     :extension "pom"
     :scope "import"]
    ;; Select non-android Guava to work around Guava versioning mess
    ;; <https://github.com/google/guava/issues/2914>.
-   [com.google.guava/guava "30.1.1-jre"]]
+   ;[com.google.guava/guava "30.1.1-jre"]
+   ]
   :dependencies
   [[org.clojure/clojure "1.10.3" :scope "provided"]
    ;; This must correspond to the version pinned in BOM files.
-   [com.google.cloud/google-cloud-core "2.3.3"]
-   [com.google.cloud/google-cloud-core-http "2.3.3"]
+   [com.google.cloud/google-cloud-core "2.6.1"]
+   [com.google.cloud/google-cloud-core-http "2.6.1"]
    ;; grpc-api is required to compile ‘clj-gcloud.common’ namespace
-   [io.grpc/grpc-api "1.42.1"]
+   [io.grpc/grpc-api "1.45.1"]
    ;; Handle version mismatches between grpc-api and other deps.
-   [com.google.errorprone/error_prone_annotations "2.10.0"]]
+   [com.google.errorprone/error_prone_annotations "2.13.1"]]
   :profiles
   {:dev
    {:dependencies
-    [[com.google.cloud/google-cloud-bigquery "2.5.1"]
-     [com.google.cloud/google-cloud-pubsub "1.115.0"]
-     [com.google.cloud/google-cloud-storage "2.2.2"]
-     [com.google.cloud/google-cloud-datastore "2.2.1"]
+    [[com.google.cloud/google-cloud-bigquery "2.10.10"]
+     [com.google.cloud/google-cloud-pubsub "1.116.4"]
+     [com.google.cloud/google-cloud-storage "2.6.1"]
+     [com.google.cloud/google-cloud-datastore "2.4.0"]
      [org.clojure/tools.namespace "1.2.0"]]
     :source-paths   ["dev"]
     :resource-paths ["test-resources"]
