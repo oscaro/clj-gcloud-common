dd544207c40dca195e4df1788d51e6a1337dd0e4
Ed Porras <dev@digressed.net>
2022-08-05 14:11:29 +0200

    bump deps (0.176.0 bom)

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.oscaro/clj-gcloud-common "0.172-2.0-SNAPSHOT"
+(defproject com.oscaro/clj-gcloud-common "0.176-1.0-SNAPSHOT"
   :description "Common library for all google cloud clojure wrappers"
   :url "https://github.com/oscaro/clj-gcloud-common"
   :license {:name "Eclipse Public License"
@@ -12,29 +12,29 @@
   :managed-dependencies
   ;; Google “Bill of Materials” (BOM) defines a combination of
   ;; dependency versions that work well with each other.
-  [[com.google.cloud/google-cloud-bom "0.172.0"
+  [[com.google.cloud/google-cloud-bom "0.176.0"
     :extension "pom"
     :scope "import"]
-   [com.google.cloud/google-cloud-shared-dependencies "2.10.0"
+   [com.google.cloud/google-cloud-shared-dependencies "3.0.1"
     :extension "pom"
     :scope "import"]]
   :dependencies
   [[org.clojure/clojure "1.11.1" :scope "provided"]
    ;; This must correspond to the version pinned in BOM files.
-   [com.google.cloud/google-cloud-core "2.6.1"]
-   [com.google.cloud/google-cloud-core-http "2.6.1"]
+   [com.google.cloud/google-cloud-core "2.8.8"]
+   [com.google.cloud/google-cloud-core-http "2.8.8"]
    ;; grpc-api is required to compile ‘clj-gcloud.common’ namespace
-   [io.grpc/grpc-api "1.45.1"]
+   [io.grpc/grpc-api "1.48.1"]
    ;; Handle version mismatches between grpc-api and other deps.
-   [com.google.errorprone/error_prone_annotations "2.13.1"]]
+   [com.google.errorprone/error_prone_annotations "2.15.0"]]
   :profiles
   {:dev
    {:dependencies
-    [[com.google.cloud/google-cloud-bigquery "2.10.10"]
-     [com.google.cloud/google-cloud-pubsub "1.116.4"]
-     [com.google.cloud/google-cloud-storage "2.6.1"]
-     [com.google.cloud/google-cloud-datastore "2.4.0"]
-     [org.clojure/tools.namespace "1.2.0"]]
+    [[com.google.cloud/google-cloud-bigquery "2.14.2"]
+     [com.google.cloud/google-cloud-pubsub "1.120.10"]
+     [com.google.cloud/google-cloud-storage "2.11.2"]
+     [com.google.cloud/google-cloud-datastore "2.10.1"]
+     [org.clojure/tools.namespace "1.3.0"]]
     :source-paths   ["dev"]
     :resource-paths ["test-resources"]
     :test-selectors {:ci (complement :integration)}}}
