e8092838cff605126493b71955a0b409612cba4b
iomonad <iomonad@riseup.net>
2023-11-02 15:04:28 +0100

    lein(deps) bump to latest dependencies

--- project.clj
+++ project.clj
@@ -50,29 +50,29 @@
   ;; which seem to correspond to
   ;;
   ;; https://mvnrepository.com/artifact/com.google.cloud/libraries-bom/26.18.0
-  [[com.google.cloud/google-cloud-bom "0.199.0"
+  [[com.google.cloud/google-cloud-bom "0.207.0"
     :extension "pom"
     :scope "import"]
-   [com.google.cloud/google-cloud-shared-dependencies "3.13.0"
+   [com.google.cloud/google-cloud-shared-dependencies "3.19.0"
     :extension "pom"
     :scope "import"]]
   :dependencies
   [[org.clojure/clojure "1.11.1" :scope "provided"]
    ;; This must correspond to the version pinned in BOM files.
-   [com.google.cloud/google-cloud-core "2.21.0"]
-   [com.google.cloud/google-cloud-core-http "2.21.0"]
+   [com.google.cloud/google-cloud-core "2.27.0"]
+   [com.google.cloud/google-cloud-core-http "2.27.0"]
    [com.google.http-client/google-http-client-gson "1.43.3"]
    ;; grpc-api is required to compile ‘clj-gcloud.common’ namespace
-   [io.grpc/grpc-api "1.56.1"]
+   [io.grpc/grpc-api "1.59.0"]
    ;; Handle version mismatches between grpc-api and other deps.
-   [com.google.errorprone/error_prone_annotations "2.20.0"]]
+   [com.google.errorprone/error_prone_annotations "2.23.0"]]
   :profiles
   {:dev
    {:dependencies
-    [[com.google.cloud/google-cloud-bigquery "2.29.0"]
-     [com.google.cloud/google-cloud-pubsub "1.123.17"]
-     [com.google.cloud/google-cloud-datastore "2.16.1"]
-     [com.google.cloud/google-cloud-storage "2.23.0"]
+    [[com.google.cloud/google-cloud-bigquery "2.34.0"]
+     [com.google.cloud/google-cloud-pubsub "1.125.11"]
+     [com.google.cloud/google-cloud-datastore "2.17.5"]
+     [com.google.cloud/google-cloud-storage "2.29.0"]
      [org.clojure/tools.namespace "1.4.4"]]
     :source-paths   ["dev" "test"]
     :resource-paths ["test-resources"]
