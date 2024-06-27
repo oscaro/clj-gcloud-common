ad3708c17e26444322aa06cba649faaae2575334
Ed Porras <dev@digressed.net>
2024-06-27 16:58:59 +0200

    Version 0.215-2.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.oscaro/clj-gcloud-common "0.215-1.0"
+(defproject com.oscaro/clj-gcloud-common "0.215-2.0-SNAPSHOT1"
   :description "Common library for all google cloud clojure wrappers"
   :url "https://github.com/oscaro/clj-gcloud-common"
   :license {:name "Eclipse Public License"
